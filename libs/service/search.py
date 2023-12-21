623f76d2e00551e8be67e31aef5546ed5da7dffb
ryosyntaxx <riodwi12174@gmail.com>
2023-12-21 17:46:09 +0700

    exstract_article completed

--- libs/service/search.py
+++ libs/service/search.py
@@ -17,17 +17,45 @@ class Research:
             "times": str(time.now()),
             "datas": []
         }
-
+        self.__base_url = 'https://www.rand.org'
         self.__user_agent = FakeUserAgent()
         self.__headers = {
             'User-Agent': self.__user_agent.random
         }
 
+
+    def complement_url(self, pieces_url: str) -> str:
+        if self.__base_url not in pieces_url:
+            return self.__base_url+pieces_url
+        return pieces_url
+
+
     def exstract_article(self, url_artc: str) -> dict:
         response = requests.get(url=url_artc, headers=self.__headers)
-        body = PyQuery(response.text)
+        html = PyQuery(response.text)
 
-        pass
+        header = html.find('#srch > article > div.post-heading')
+        footer = html.find('#srch > article > div.constrain-width')
+        
+        results = {
+            "source": self.__parser.ex(html=header, selector="p.source").text(),
+            "author": {
+                "name": self.__parser.ex(html=footer, selector="p.authors > a").text(),
+                "profil": self.complement_url(self.__parser.ex(html=footer, selector="p.authors > a").attr('href')),
+                "position": self.__parser.ex(html=footer, selector="div.blog-column-left h4").text(),
+                "username": self.__parser.ex(html=footer, selector="div.blog-column-left p > a").text(),
+                "contact": self.__parser.ex(html=footer, selector="div.blog-column-left p > a").attr('href')
+            },
+            "topic": [{
+                "blog": self.complement_url(self.__parser.ex(html=tag, selector="a").attr('href')),
+                "tags": self.__parser.ex(html=tag, selector="a").text(),
+            } for tag in self.__parser.ex(html=footer, selector="div.blog-column-right ul > li")],
+            "Article": self.__parser.ex(html=footer, selector="div.body-text p").text()
+        }
+
+        self.__writer.ex(path='private/artc.json', content=results)
+        ic(results)
+        
 
     def exstract_data(self, pieces_table: str):
             
@@ -38,12 +66,13 @@ class Research:
             "descriptions": self.__parser.ex(html=pieces_table, selector='div.text p.desc').text(),
             "posted": self.__parser.ex(html=pieces_table, selector='div.text p.date').text(),
             "image": {
-                "thumb": self.__parser.ex(html=pieces_table, selector='div.img-wrap a img').attr('src'),
+                "thumb": self.complement_url(self.__parser.ex(html=pieces_table, selector='div.img-wrap a img').attr('src')),
                 "desc": self.__parser.ex(html=pieces_table, selector='div.img-wrap a img').attr('alt'),
-            }
+            },
+            "content": self.exstract_article(url_artc=self.__parser.ex(html=pieces_table, selector='div.text h3.title a').attr('href'))
         }
 
-        self.__writer.ex(path='private/news.json', content=self.__results)
+        self.__writer.ex(path='private/results.json', content=results)
 
         return results
 
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from libs import Downloader
 
 if __name__ == '__main__':
     search = Research()
-    search.exstract_url(page_url='kk')
+    # search.exstract_article(url_artc='https://www.rand.org/pubs/commentary/2023/12/vietnams-show-of-welcome-for-xi-reflects-growing-self.html')
+    search.execute()
 
     # download = Downloader()
     # download.download(url="https://www.rand.org/content/dam/rand/pubs/testimonies/CTA3000/CTA3097-1/RAND_CTA3097-1.pdf")
